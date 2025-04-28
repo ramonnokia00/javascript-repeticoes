b46764687c697b3f4a55dd9188966daa38e8ecde
unknown <ramonsantos6053@digitalcollege.internal>
2025-04-28 16:48:25 -0300

    primeiro-commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function contagem(){
+    let voltasValue = Number(document.querySelector("#voltas").value);
+    
+    for(let i = 0; i <= voltas.value; i++){
+    console.log(i);
+    
+}
+}
+function tabuada(){
+    let numeroValue = Number(document.querySelector("#numero").value);
+    
+    for(let i = 0; i <= 10; i++){
+    console.log(`${numeroValue} X ${i} = ${numeroValue * i}`);
+    
+}
+}
